2f474af049d03553333f68124387d796afadf23e
Will Forcey <wsforc3y@gmail.com>
2021-08-25 14:09:47 -0400

    Add files via upload

--- main.go
+++ main.go
@@ -4,45 +4,76 @@ import (
   "fmt"
   "net/http"
   "io"
+  "io/ioutil"
   "strings"
   "flag"
 )
 
 func main(){
   url := flag.String("url", "", "url of youtube playlist")
+  file := flag.String("file", "", "path to file from youtube view-source (or just file name if the file is in this directory)")
 	flag.Parse()
 
-  if *url == "" {
+  if *url == "" && *file == "" {
 		fmt.Println("Usage example: main.exe -url https://www.youtube.com/playlist?list=PL96C35uN7xGLLeET0dOWaKHkAlPsrkcha")
-		return
+    fmt.Println("Usage example: main.exe -file myMusic.html")
+    return
 	}
 
+  var b []byte  //Stores byte text data
+  var err error
 
-  resp, err := http.Get(*url)
-  if err != nil {
-    panic(err)
+  if *file != "" {
+    b, err = ioutil.ReadFile(*file)
+    if err != nil {
+      panic(err)
+    }
+  }else{
+    resp, err := http.Get(*url)
+    if err != nil {
+      panic(err)
+    }
+    defer resp.Body.Close()
+
+    b, err = io.ReadAll(resp.Body)
+    if err != nil {
+      panic(err)
+    }
   }
-  defer resp.Body.Close()
 
-  b, err := io.ReadAll(resp.Body)
-  if err != nil {
-    panic(err)
-  }
 
-  body := string(b)
+
 
   //fmt.Println(body)
 
   var vidTitles []string
   var match string = "\"title\":{\"runs\":[{\"text\":\""
+  var match2 string = "\"accessibility\":{\"accessibilityData\":{\"label\":\""
+  //var match string = "aria-label"
 
+  body := string(b)  //copy of body to work on
 
   for{
     i := strings.Index(body, match)
-    if i == -1 {break}
+    if i != -1 {
+      //delete everything before that point in the string body
+      body = body[i+len(match):]
 
-    //delete everything before that point in the string body
-    body = body[i+len(match):]
+      titleEnd := strings.Index(body, "\"")
+      if titleEnd == -1 {break}
+
+      //else add title to vidTitles
+      vidTitles = append(vidTitles, body[:titleEnd])
+
+      //finally delete that title from main string
+      body = body[titleEnd:]
+
+    }else{
+      break
+    }
+
+
+
 /*
     titleStart := strings.Index(body, "title")
     if titleStart == -1 {break}
@@ -51,17 +82,33 @@ func main(){
 
     body = body[titleStart:]
 */
-    titleEnd := strings.Index(body, "\"")
-    if titleEnd == -1 {break}
 
-    //else add title to vidTitles
-    vidTitles = append(vidTitles, body[:titleEnd])
-
-    //finally delete that title from main string
-    body = body[titleEnd:]
-
+  //  fmt.Println(body, "\n")
 
   }
+
+  body = string(b) //fresh copy of body
+
+  for {
+    i := strings.Index(body, match2)
+    if i != -1 {
+      //delete everything before that point in the string body
+      body = body[i+len(match2):]
+
+      titleEnd := strings.Index(body, "\"")
+      if titleEnd == -1 {break}
+
+      //else add title to vidTitles
+      vidTitles = append(vidTitles, body[:titleEnd])
+
+      //finally delete that title from main string
+      body = body[titleEnd:]
+    }else{
+      break
+    }
+  }
+
+
 
   for i:= 0; i<len(vidTitles); i++ {
     fmt.Println(vidTitles[i])
