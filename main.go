0a3f295515909af05eefa35a1a9b09c7a2048ed5
Will Forcey <wsforc3y@gmail.com>
2021-08-25 13:35:16 -0400

    Add files via upload

--- main.go
+++ main.go
@@ -29,20 +29,36 @@ func main(){
     panic(err)
   }
 
-  body := string(b)
-
   //fmt.Println(body)
 
   var vidTitles []string
   var match string = "\"title\":{\"runs\":[{\"text\":\""
+  var match2 string = "\"accessibility\":{\"accessibilityData\":{\"label\":\""
+  //var match string = "aria-label"
 
+  body := string(b)  //copy of body to work on
 
   for{
     i := strings.Index(body, match)
-    if i == -1 {break}
+    if i != -1 {
+      //delete everything before that point in the string body
+      body = body[i+len(match):]
 
-    //delete everything before that point in the string body
-    body = body[i+len(match):]
+      titleEnd := strings.Index(body, "\"")
+      if titleEnd == -1 {break}
+
+      //else add title to vidTitles
+      vidTitles = append(vidTitles, body[:titleEnd])
+
+      //finally delete that title from main string
+      body = body[titleEnd:]
+
+    }else{
+      break
+    }
+
+
+
 /*
     titleStart := strings.Index(body, "title")
     if titleStart == -1 {break}
@@ -51,17 +67,33 @@ func main(){
 
     body = body[titleStart:]
 */
-    titleEnd := strings.Index(body, "\"")
-    if titleEnd == -1 {break}
 
-    //else add title to vidTitles
-    vidTitles = append(vidTitles, body[:titleEnd])
-
-    //finally delete that title from main string
-    body = body[titleEnd:]
-
+  //  fmt.Println(body, "\n")
 
   }
+
+  body = string(b) //fresh copy of body
+
+  for {
+    i := strings.Index(body, match2)
+    if i != -1 {
+      //delete everything before that point in the string body
+      body = body[i+len(match2):]
+
+      titleEnd := strings.Index(body, "\"")
+      if titleEnd == -1 {break}
+
+      //else add title to vidTitles
+      vidTitles = append(vidTitles, body[:titleEnd])
+
+      //finally delete that title from main string
+      body = body[titleEnd:]
+    }else{
+      break
+    }
+  }
+
+
 
   for i:= 0; i<len(vidTitles); i++ {
     fmt.Println(vidTitles[i])
