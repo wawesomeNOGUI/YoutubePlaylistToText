57443ebf925c6d14878eb77eee19aede347690da
Will Forcey <wsforc3y@gmail.com>
2024-04-06 22:22:32 -0400

    updated youtube parsing
    now gets song title : artist name

--- main.go
+++ main.go
@@ -1,108 +1,113 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "io"
-  "io/ioutil"
-  "strings"
-  "flag"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
-func main(){
-  url := flag.String("url", "", "url of youtube playlist")
-  file := flag.String("file", "", "path to file from youtube view-source (or just file name if the file is in this directory)")
+func main() {
+	url := flag.String("url", "", "url of youtube playlist")
+	file := flag.String("file", "", "path to file from youtube view-source (or just file name if the file is in this directory)")
 	flag.Parse()
 
-  if *url == "" && *file == "" {
+	if *url == "" && *file == "" {
 		fmt.Println("Usage example: main.exe -url https://www.youtube.com/playlist?list=PL96C35uN7xGLLeET0dOWaKHkAlPsrkcha")
-    fmt.Println("Usage example: main.exe -file myMusic.html")
-    return
+		fmt.Println("Usage example: main.exe -file myMusic.html")
+		return
 	}
 
-  var b []byte  //Stores byte text data
-  var err error
+	var b []byte //Stores byte text data
+	var err error
 
-  if *file != "" {
-    b, err = ioutil.ReadFile(*file)
-    if err != nil {
-      panic(err)
-    }
-  }else{
-    resp, err := http.Get(*url)
-    if err != nil {
-      panic(err)
-    }
-    defer resp.Body.Close()
+	if *file != "" {
+		b, err = ioutil.ReadFile(*file)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		resp, err := http.Get(*url)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
 
-    b, err = io.ReadAll(resp.Body)
-    if err != nil {
-      panic(err)
-    }
-  }
+		b, err = io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+	}
 
+	//fmt.Println(body)
 
+	var vidTitles []string
+	match := "title=\""
+	//var match string = "\"title\":{\"runs\":[{\"text\":\""
+	//var match2 string = "\"accessibility\":{\"accessibilityData\":{\"label\":\""
+	//var match2 string = "aria-label=\""
 
+	body := string(b) //copy of body to work on
 
-  //fmt.Println(body)
+	// update april 2024
+	// looks like the format is title="song-title" .... .... title="artist-name/channel name"
+	// so I'll try to format like that when printing out vidTitles
+	for {
+		i := strings.Index(body, match)
+		if i != -1 {
+			//delete everything before that point in the string body
+			body = body[i+len(match):]
 
-  var vidTitles []string
-  match2 :=  "aria-label=\""
-  match := "title=\""
-  //var match string = "\"title\":{\"runs\":[{\"text\":\""
-  //var match2 string = "\"accessibility\":{\"accessibilityData\":{\"label\":\""
-  //var match2 string = "aria-label=\""
+			titleEnd := strings.Index(body, "\"")
+			if titleEnd == -1 {
+				break
+			}
 
-  body := string(b)  //copy of body to work on
+			//else add title to vidTitles
+			vidTitles = append(vidTitles, body[:titleEnd])
 
-  for{
-    i := strings.Index(body, match)
-    if i != -1 {
-      //delete everything before that point in the string body
-      body = body[i+len(match):]
+			//finally delete that title from main string
+			body = body[titleEnd:]
 
-      titleEnd := strings.Index(body, "\"")
-      if titleEnd == -1 {break}
+		} else {
+			break
+		}
+	}
 
-      //else add title to vidTitles
-      vidTitles = append(vidTitles, body[:titleEnd])
+	//First Print Playlist Length
+	fmt.Println(len(vidTitles) / 2, " Items In Playlist")
 
-      //finally delete that title from main string
-      body = body[titleEnd:]
+	// body = string(b) //fresh copy of body
 
-    }else{
-      break
-    }
-  }
+	// match2 := "aria-label=\""
 
-  //First Print Playlist Length
-  fmt.Println(len(vidTitles), " Items In Playlist")
+	// for {
+	// 	i := strings.Index(body, match2)
+	// 	if i != -1 {
+	// 		//delete everything before that point in the string body
+	// 		body = body[i+len(match2):]
 
-  body = string(b) //fresh copy of body
+	// 		titleEnd := strings.Index(body, "\"")
+	// 		if titleEnd == -1 {
+	// 			break
+	// 		}
 
-  for {
-    i := strings.Index(body, match2)
-    if i != -1 {
-      //delete everything before that point in the string body
-      body = body[i+len(match2):]
+	// 		//else add title to vidTitles
+	// 		vidTitles = append(vidTitles, body[:titleEnd])
 
-      titleEnd := strings.Index(body, "\"")
-      if titleEnd == -1 {break}
+	// 		//finally delete that title from main string
+	// 		body = body[titleEnd:]
+	// 	} else {
+	// 		break
+	// 	}
+	// }
 
-      //else add title to vidTitles
-      vidTitles = append(vidTitles, body[:titleEnd])
-
-      //finally delete that title from main string
-      body = body[titleEnd:]
-    }else{
-      break
-    }
-  }
-
-  //Print all the titles
-  for i:= 0; i<len(vidTitles); i++ {
-    fmt.Println(vidTitles[i])
-  }
-
+	//Print all the titles
+	for i := 0; i < len(vidTitles); i += 2 {
+		// list song name : artist name
+		fmt.Println(vidTitles[i], ": ", vidTitles[i+1])
+	}
 
 }
